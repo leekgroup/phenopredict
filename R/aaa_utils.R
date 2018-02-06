589868e4781dcdd45448a7743d654a732601fc2e
ShanEllis <sellis18@jhmi.edu>
2018-02-06 14:36:15 -0500

    Adding Appveyor.

--- R/aaa_utils.R
+++ R/aaa_utils.R
@@ -1,10 +1,14 @@
 #' @importFrom quadprog solve.QP
-projectCellType = function (Y, coefCellType, contrastCellType = NULL, nonnegative = TRUE,
-          lessThanOne = FALSE)
+projectCellType = function (Y,
+                            coefCellType,
+                            contrastCellType = NULL,
+                            nonnegative = TRUE,
+                            lessThanOne = FALSE)
 {
   if (is.null(contrastCellType))
     Xmat <- coefCellType
-  else Xmat <- tcrossprod(coefCellType, contrastCellType)
+  else
+    Xmat <- tcrossprod(coefCellType, contrastCellType)
   nCol <- dim(Xmat)[2]
   if (nCol == 2) {
     Dmat <- crossprod(Xmat)
@@ -30,17 +34,18 @@ projectCellType = function (Y, coefCellType, contrastCellType = NULL, nonnegativ
       }
       for (i in 1:nSubj) {
         obs <- which(!is.na(Y[, i]))
-        Dmat <- crossprod(Xmat[obs, ])
-        mixCoef[i, ] <- solve.QP(Dmat, crossprod(Xmat[obs,
-                                                      ], Y[obs, i]), Amat, b0vec)$sol
+        Dmat <- crossprod(Xmat[obs,])
+        mixCoef[i,] <- solve.QP(Dmat,
+                                crossprod(Xmat[obs,],
+                                          Y[obs, i]), Amat, b0vec)$sol
       }
     }
     else {
       for (i in 1:nSubj) {
         obs <- which(!is.na(Y[, i]))
-        Dmat <- crossprod(Xmat[obs, ])
-        mixCoef[i, ] <- solve(Dmat, t(Xmat[obs, ]) %*%
-                                Y[obs, i])
+        Dmat <- crossprod(Xmat[obs,])
+        mixCoef[i,] <- solve(Dmat, t(Xmat[obs,]) %*%
+                               Y[obs, i])
       }
     }
     return(mixCoef)
@@ -50,8 +55,12 @@ projectCellType = function (Y, coefCellType, contrastCellType = NULL, nonnegativ
 #' @importFrom stats model.matrix lm pf vcov
 #' @importFrom nlme lme getVarCov
 #'
-validationCellType = function (Y, pheno, modelFix, modelBatch = NULL, L.forFstat = NULL,
-          verbose = FALSE)
+validationCellType = function (Y,
+                               pheno,
+                               modelFix,
+                               modelBatch = NULL,
+                               L.forFstat = NULL,
+                               verbose = FALSE)
 {
   N <- dim(pheno)[1]
   pheno$y <- rep(0, N)
@@ -59,31 +68,33 @@ validationCellType = function (Y, pheno, modelFix, modelBatch = NULL, L.forFstat
   sizeModel <- dim(xTest)[2]
   M <- dim(Y)[1]
   if (is.null(L.forFstat)) {
-    L.forFstat <- diag(sizeModel)[-1, ]
+    L.forFstat <- diag(sizeModel)[-1,]
     colnames(L.forFstat) <- colnames(xTest)
     rownames(L.forFstat) <- colnames(xTest)[-1]
   }
-  sigmaResid <- sigmaIcept <- nObserved <- nClusters <- Fstat <- rep(NA,
-                                                                     M)
+  sigmaResid <-
+    sigmaIcept <- nObserved <- nClusters <- Fstat <- rep(NA,
+                                                         M)
   coefEsts <- matrix(NA, M, sizeModel)
   coefVcovs <- list()
   if (verbose)
     cat("[validationCellType] ")
   for (j in 1:M) {
-    ii <- !is.na(Y[j, ])
+    ii <- !is.na(Y[j,])
     nObserved[j] <- sum(ii)
-    pheno$y <- Y[j, ]
-    if (j%%round(M/10) == 0 && verbose)
+    pheno$y <- Y[j,]
+    if (j %% round(M / 10) == 0 && verbose)
       cat(".")
     try({
       if (!is.null(modelBatch)) {
         fit <- try(lme(modelFix, random = modelBatch,
-                       data = pheno[ii, ]))
+                       data = pheno[ii,]))
         OLS <- inherits(fit, "try-error")
       }
-      else OLS <- TRUE
+      else
+        OLS <- TRUE
       if (OLS) {
-        fit <- lm(modelFix, data = pheno[ii, ])
+        fit <- lm(modelFix, data = pheno[ii,])
         fitCoef <- fit$coef
         sigmaResid[j] <- summary(fit)$sigma
         sigmaIcept[j] <- 0
@@ -95,11 +106,11 @@ validationCellType = function (Y, pheno, modelFix, modelBatch = NULL, L.forFstat
         sigmaIcept[j] <- sqrt(getVarCov(fit)[1])
         nClusters[j] <- length(fit$coef$random[[1]])
       }
-      coefEsts[j, ] <- fitCoef
+      coefEsts[j,] <- fitCoef
       coefVcovs[[j]] <- vcov(fit)
       useCoef <- L.forFstat %*% fitCoef
       useV <- L.forFstat %*% coefVcovs[[j]] %*% t(L.forFstat)
-      Fstat[j] <- (t(useCoef) %*% solve(useV, useCoef))/sizeModel
+      Fstat[j] <- (t(useCoef) %*% solve(useV, useCoef)) / sizeModel
     })
   }
   if (verbose)
@@ -108,10 +119,21 @@ validationCellType = function (Y, pheno, modelFix, modelBatch = NULL, L.forFstat
   colnames(coefEsts) <- names(fitCoef)
   degFree <- nObserved - nClusters - sizeModel + 1
   Pval <- 1 - pf(Fstat, sizeModel, degFree)
-  out <- list(coefEsts = coefEsts, coefVcovs = coefVcovs, modelFix = modelFix,
-              modelBatch = modelBatch, sigmaIcept = sigmaIcept, sigmaResid = sigmaResid,
-              L.forFstat = L.forFstat, Pval = Pval, orderFstat = order(-Fstat),
-              Fstat = Fstat, nClusters = nClusters, nObserved = nObserved,
-              degFree = degFree)
+  out <-
+    list(
+      coefEsts = coefEsts,
+      coefVcovs = coefVcovs,
+      modelFix = modelFix,
+      modelBatch = modelBatch,
+      sigmaIcept = sigmaIcept,
+      sigmaResid = sigmaResid,
+      L.forFstat = L.forFstat,
+      Pval = Pval,
+      orderFstat = order(-Fstat),
+      Fstat = Fstat,
+      nClusters = nClusters,
+      nObserved = nObserved,
+      degFree = degFree
+    )
   out
 }
